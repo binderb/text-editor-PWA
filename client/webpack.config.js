455446f2964904849fac453c736dbf6d1e077aee
Ben Binder <ben.binder11@gmail.com>
2023-01-21 17:12:35 -0600

    Add missing webpack config.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,6 +5,9 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+
+
 
 module.exports = () => {
   return {
@@ -18,12 +21,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              // plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
