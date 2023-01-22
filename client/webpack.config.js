cdbd05820dcc7c2cb5c7670e4bdabc4a9abff128
Ben Binder <ben.binder11@gmail.com>
2023-01-22 10:24:36 -0600

    Add webpack manifest.json generation.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,6 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 
@@ -21,13 +20,39 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Plugin to generate HTML file.
       new HtmlWebpackPlugin({
         template: './index.html',
       }),
+      // Plugin to create custom service worker.
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'service-worker.js'
-      })
+      }),
+      // Plugin to create manifest.json file.
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Write code, even offline!',
+        orientation: 'portrait',
+        display: 'standalone',
+        start_url: './',
+        crossorigin: null,
+        inject: true,
+        fingerprints: false,
+        publicPath: './',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        ios: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
