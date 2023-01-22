c8c26b5d9dc52a095ef7c982c31a4db4bd392a57
Ben Binder <ben.binder11@gmail.com>
2023-01-22 15:02:41 -0600

    Implement GET and PUT functionality for IndexedDB.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,14 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO (Done): Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const store = jateDb.transaction('jate', 'readwrite').objectStore('jate');
+  // This will either update the row with id=1, or create the record if it doesn't already exist. Since the text editor doesn't have any capability to revert to previous versions of the text, there's no reason to store more than a single row. 
+  const request = store.put({id: 1, jate_content: content});
+  const result = await request;
+  console.log(`User content has been saved.`);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// TODO (Done): Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const store = jateDb.transaction('jate', 'readonly').objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('Attempt to load content from database returned: ', result);
+  return result;
 }
 
 initdb();
